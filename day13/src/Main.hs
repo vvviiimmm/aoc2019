cba8a93c3954c7e43eef58f3b6a797d7244dbc2c
Alexey Avramenko <alexey.awramenko@gmail.com>
2020-01-03 19:40:00 +0800

    Cleanups

--- day13/src/Main.hs
+++ day13/src/Main.hs
@@ -5,26 +5,30 @@
 
 module Main where
 
-import IntExecution
-import IntSystem
-
-import Control.Monad.State.Strict as State
-import Data.List.Split
-import qualified Data.Map as Map
-import Data.Maybe
-import qualified Data.Set as Set
-import Control.Lens
-import System.Console.ANSI
+import Data.List.Split (splitOn)
+import Data.Maybe (fromMaybe)
 import System.IO (hFlush, stdout)
 import Control.Concurrent (threadDelay)
+
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import  Control.Monad.State.Strict as State
+
+import Control.Lens
+       ((%=), (%~), (&), (+=), (.=), (.~), (^.), makeLenses)
 import Data.Colour.SRGB (sRGB24)
-import Debug.Trace
+
+import qualified System.Console.ANSI as ANSI
+
+import IntExecution
+import IntSystem
 
 initSystemForInput :: [MWord] -> [MWord] -> System
 initSystemForInput program input = system
   where
     initialMemory = Map.fromList $ zip (fmap Addr [0 ..]) program
-    initRegisters = Registers {_relativeBase = 0, _ip = 0, _halt = False, _tick = 0}
+    initRegisters =
+      Registers {_relativeBase = 0, _ip = 0, _halt = False, _tick = 0}
     system = System initialMemory initRegisters input [] []
 
 -- Day 13
@@ -35,35 +39,23 @@ data Coord = Coord
 
 data Tile
   = EmptyTile
-  | Wall 
+  | Wall
   | Block
   | Paddle
-  | Ball deriving (Show, Enum,Eq)
-
-data Direction
-  = DUp
-  | DRight
-  | DDown
-  | DLeft
-  deriving (Show, Enum)
-
-data Turn
-  = CCW
-  | CW
-  deriving (Show, Enum)
+  | Ball
+  deriving (Show, Enum, Eq)
 
 data JoyPosition
   = Neutral
   | JLeft
-  | JRight deriving (Show, Eq)
+  | JRight
+  deriving (Show, Eq)
 
 type Field = Map.Map Coord Tile
 
 data SystemWithScreen = SystemWithScreen
   { _system :: System
   , _field :: Field
-  , _cursor :: Coord
-  , _cursorDirection :: Direction
   , _score :: Integer
   , _paddlePosition :: Integer
   , _joyPosition :: JoyPosition
@@ -71,65 +63,53 @@ data SystemWithScreen = SystemWithScreen
 
 makeLenses ''SystemWithScreen
 
-systemWithScreenStep :: StateT SystemWithScreen IO ()
-systemWithScreenStep = do
-  sws <- get
+ballTileId = 4
+scoreTileX = -1
+scoreTileY = 0
 
-  let terminationPredicate sys = length (sys^.output) == 3 || sys^.registers.halt
-      intMachineWithOutput = State.execState (runUntil terminationPredicate) (sws^.system)
-  if null (intMachineWithOutput^.output)
+systemWithScreenStep :: State.StateT SystemWithScreen IO ()
+systemWithScreenStep = do
+  sws <- State.get
+  let terminationPredicate sys =
+        length (sys ^. output) == 3 || sys ^. registers . halt
+      intMachineWithOutput =
+        State.execState (runUntil terminationPredicate) (sws ^. system)
+  if null (intMachineWithOutput ^. output)
     then pure ()
     else do
-      let [tx, ty, tid] = intMachineWithOutput^.output
-          newJoyPosition = if tid == 4 then
-            joyDirection (sws^.paddlePosition) tx
-              else
-            sws^.joyPosition
+      let [tx, ty, tid] = intMachineWithOutput ^. output
+          newJoyPosition =
+            if tid == ballTileId
+              then joyDirection (sws ^. paddlePosition) tx
+              else sws ^. joyPosition
           newInputs = [fromJoyPosition newJoyPosition]
-          
-      if tx == -1 && ty == 0 then
-        score .= tid
-      else do
-        if (sws^.system^.registers^.tick > 15000) then liftIO (threadDelay 5000) else pure ()
-        paintPixel (Coord tx ty) (toEnum (fromInteger tid))
-
+      if tx == scoreTileX && ty == scoreTileY 
+        then score .= tid
+        else paintPixel (Coord tx ty) (toEnum (fromInteger tid))
       system .= (intMachineWithOutput & (output .~ []) & (input .~ newInputs))
-      paddlePosition += (fromJoyPosition newJoyPosition) 
+      paddlePosition += fromJoyPosition newJoyPosition
       systemWithScreenStep
 
-paintPixel :: Coord -> Tile -> StateT SystemWithScreen IO ()
+paintPixel :: Coord -> Tile -> State.StateT SystemWithScreen IO ()
 paintPixel coord tile = do
   field %= Map.insert coord tile
-  liftIO $ paintPixelAscii coord tile
-
-drawScreen :: Field -> [String]
-drawScreen f = grouped 51 l' where 
-  indices = [Coord x y | y <- [0 .. 25], x <- [0 .. 50]]
-  l' = fmap (\c -> colorToChar $ colorAt c f)  indices
-
-drawScreenAscii :: Field -> IO ()
-drawScreenAscii = undefined
+  State.liftIO $ paintPixelAscii coord tile
 
 paintPixelAscii :: Coord -> Tile -> IO ()
 paintPixelAscii (Coord y x) tile = do
-  setCursorPosition (fromIntegral x) (fromIntegral y)
-  let cx = 22
-      cy = 0
-      r = fromIntegral $ 9 * x
+  ANSI.setCursorPosition (fromIntegral x) (fromIntegral y)
+  let r = fromIntegral $ 9 * x
       g = 0
-      b = 102 -- (fromIntegral x) * 5
-  setSGR [SetRGBColor Background $ sRGB24 r g b]
-  putChar (colorToChar tile)
-  
-colorToChar :: Tile -> Char
-colorToChar EmptyTile = ' '
-colorToChar Wall = '░'
-colorToChar Block = '▂'
-colorToChar Paddle = '▄'
-colorToChar Ball = '•'
+      b = 102
+  ANSI.setSGR [ANSI.SetRGBColor ANSI.Background $ sRGB24 r g b]
+  putChar (tileToChar tile)
 
-colorAt :: Coord -> Field -> Tile
-colorAt c f = fromMaybe EmptyTile $ Map.lookup c f
+tileToChar :: Tile -> Char
+tileToChar EmptyTile = ' '
+tileToChar Wall = '░'
+tileToChar Block = '▂'
+tileToChar Paddle = '▄'
+tileToChar Ball = '•'
 
 joyDirection :: Integer -> Integer -> JoyPosition
 joyDirection paddleX ballX
@@ -139,7 +119,7 @@ joyDirection paddleX ballX
 
 toJoyPosition :: Integer -> JoyPosition
 toJoyPosition 0 = Neutral
-toJoyPosition (-1) = JLeft 
+toJoyPosition (-1) = JLeft
 toJoyPosition 1 = JRight
 
 fromJoyPosition :: JoyPosition -> Integer
@@ -147,11 +127,10 @@ fromJoyPosition Neutral = 0
 fromJoyPosition JLeft = -1
 fromJoyPosition JRight = 1
 
-grouped :: Int -> [a] -> [[a]]
-grouped 1 xs = [xs]
-grouped _ [] = []
-grouped n xs = take n xs : grouped n (drop n xs)
-
+resetScreen :: IO ()
+resetScreen =
+  ANSI.setSGR [ANSI.Reset] >> ANSI.clearScreen >> ANSI.hideCursor >>
+  ANSI.setCursorPosition 0 0
 
 part1 :: IO ()
 part1 = do
@@ -162,14 +141,12 @@ part1 = do
         SystemWithScreen
         { _system = initSystem
         , _field = Map.singleton (Coord 0 0) EmptyTile
-        , _cursor = Coord 0 0
-        , _cursorDirection = DUp
         , _paddlePosition = 0
         , _joyPosition = Neutral
         , _score = 0
         }
   result <- State.execStateT systemWithScreenStep initSystemWithScreen
-  print (length $ Map.filter (== Block) (result^.field) )
+  print (length $ Map.filter (== Block) (result ^. field))
 
 part2 :: IO ()
 part2 = do
@@ -180,34 +157,17 @@ part2 = do
         SystemWithScreen
         { _system = (memory %~ Map.insert (Addr 0) 2) initSystem
         , _field = Map.singleton (Coord 0 0) EmptyTile
-        , _cursor = Coord 0 0
-        , _cursorDirection = DUp
         , _paddlePosition = 20
         , _joyPosition = Neutral
         , _score = 0
         }
-  
-  -- extract
   resetScreen
-  hideCursor
-  -- setSGR [SetRGBColor Background $ sRGB24  g b]
-
   result <- State.execStateT systemWithScreenStep initSystemWithScreen
-  
-  restoreCursor
+  ANSI.restoreCursor
   putStrLn ""
-  putStrLn $ ("Score: " <> (show (result^.score)) )
-  -- forM_ (drawScreen (result^.field)) putStrLn
-
+  putStrLn $ "Score: " <> show (result ^. score)
 
 main :: IO ()
 main = part2
 
-pause :: IO ()
-pause = do
-  hFlush stdout
-  -- 1 second pause
-  threadDelay 1000000
 
-resetScreen :: IO ()
-resetScreen = setSGR [Reset] >> clearScreen >> setCursorPosition 0 0
